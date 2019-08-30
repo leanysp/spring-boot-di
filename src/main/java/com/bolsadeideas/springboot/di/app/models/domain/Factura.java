ab302a79b6ba7a3846ccab03197a17161c7a1bc8
leanysp <leanysp@gmail.com>
2019-08-30 18:19:17 -0500

    @PostConstruct y @PreDestroy

--- src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -2,6 +2,9 @@ package com.bolsadeideas.springboot.di.app.models.domain;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -17,6 +20,17 @@ public class Factura {
 	
 	@Autowired
 	private List<ItemFactura> items;
+	
+	@PostConstruct//modificamos datos
+	public void inicializar() {
+		cliente.setNombre(cliente.getNombre().concat(" ").concat("Candelaria"));
+		descripcion=descripcion.concat(" del cliente: ").concat(cliente.getNombre());
+	}
+	
+	@PreDestroy 
+	public void destruir() {
+		System.out.println("Factura destruida: ".concat(descripcion));   
+	}
 
 	public String getDescripcion() {
 		return descripcion;
